0ee461cfbc3b24a12145322ebe0d086c2a5ad9e7
Pythagore <pythagoreadjalla@gmail.com>
2025-08-10 21:50:17 +0100

    Modifications fichiers config

--- src/config/config.py
+++ src/config/config.py
@@ -1,19 +1,8 @@
-
-import os
 from dotenv import load_dotenv
+import os
 
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-dotenv_path = os.path.join(project_root, '.env')
-load_dotenv(dotenv_path=dotenv_path)
-
-
-class Config:
-    
-    SUPABASE_URL: str = os.getenv("SUPABASE_URL")
-    SUPABASE_KEY: str = os.getenv("SUPABASE_API_KEY")
-    TABLE_CACHER: str = "cacher"
-    TABLE_PRODUCTS: str = "products"
-config = Config()
+load_dotenv()
 
 
-
+SUPABSE_URL = os.getenv("SUPABSE_URL", "")
+SUPABASE_KEY = os.getenv("SUPABASE_API_KEY", "")
