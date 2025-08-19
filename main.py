3d9f8be503b90b2cc973f7799b29f99cefe87fd5
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2025-08-19 09:51:37 +0100

    api updated

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import uvicorn
 from src.api import app
+from src.config.config import ENV
 
 
 def main():
     print("Hello from product-scraper-system!")
-    uvicorn.run(app.app)
+    uvicorn.run("src.api.app:app", host="0.0.0.0", reload=ENV == "dev")
 
 
 if __name__ == "__main__":
